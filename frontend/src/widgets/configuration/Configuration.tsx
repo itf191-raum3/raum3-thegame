d5fbd19dcd3b1bb1ace09bbbf5d2875b3121ee31
Timo <Timo.Theissen@outlook.com>
2021-05-05 11:58:39 +0200

    Added configuration gui

--- frontend/src/widgets/configuration/Configuration.tsx
+++ frontend/src/widgets/configuration/Configuration.tsx
@@ -1,3 +1,6 @@
+import deleteIcon from './delete_icon.png';
+import editIcon from './edit_icon.png';
+import './Configuration.css'
 import { AppBar, Tab, Tabs } from "@material-ui/core";
 
 
@@ -17,6 +20,26 @@ function Configuration() {
           </Tabs>
           </AppBar>
 
+          <div>
+            <table>
+              <tr>
+                <th>Id</th>
+                <th>Aufgabe</th>
+                <th>Richtige Antworten</th>
+                <th>Antwortmöglichkeiten</th>
+                <th>Löschen</th>
+                <th>Bearbeiten</th>
+              </tr>
+              <tr id="1">
+                <td className="id"></td>
+                <td className="exercise"></td>
+                <td className="correctAnswers"></td>
+                <td className="allChoices"></td>
+                <td className="deleteBnt"><button><img src={deleteIcon} alt="Löschen" className="bntLogo"/></button></td>
+                <td className="editBnt"><button><img src={editIcon} alt="Bearbeiten" className="bntLogo"/></button></td>
+              </tr>
+            </table>
+          </div>
         </div>
       </body>
     </div>
