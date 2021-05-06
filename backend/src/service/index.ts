9a4679838c4d1e566fff0991b20c17593a534978
Justin Braun <justinbraun1999@web.de>
2021-05-06 14:39:51 +0200

    * Adding testing stuff * TTYPESCRIPT!

--- backend/src/service/index.ts
+++ backend/src/service/index.ts
@@ -1,7 +1,17 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
+import {Exercise} from "@/entities/Exercise";
 
-createConnection().then(connection => {
-
+createConnection().then(async connection => {
+    const test = connection.manager.create<Exercise>(Exercise, {
+        id: 1,
+        correctAnswers: ["right1", "right2"],
+        label:"bloop",
+    });
+    await connection.manager.save(test);
+    await connection.manager.find<Exercise>(Exercise).then(e => {
+        e.forEach(result => {
+            console.log(result.correctAnswers[1]);
+        })
+    })
 });
-
