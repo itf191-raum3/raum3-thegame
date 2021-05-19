57b140938adfba67241f0cc41e2204df8b250b52
Justin Braun <justinbraun1999@web.de>
2021-05-19 11:47:42 +0200

    * Added json to Choice and Cloze

--- backend/src/entities/Cloze.ts
+++ backend/src/entities/Cloze.ts
@@ -1,9 +1,10 @@
 import {ChildEntity, Column} from "typeorm";
 import {Exercise} from "@/entities/Exercise";
+import {ICloze} from "@common/entities/ICloze";
 
 @ChildEntity()
-export class Cloze extends Exercise {
-    @Column()
+export class Cloze extends Exercise implements ICloze {
+    @Column("json")
     options: Array<string>;
 
     @Column()
