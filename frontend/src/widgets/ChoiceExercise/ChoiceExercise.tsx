4a138a5857034f26ef20cb8e29b986f4314095aa
Niko Fahl <niko.fahl@inform-software.com>
2021-05-20 11:42:04 +0200

    feat: first commit for the Choice Exercise

--- frontend/src/widgets/ChoiceExercise/ChoiceExercise.tsx
+++ frontend/src/widgets/ChoiceExercise/ChoiceExercise.tsx
@@ -1,10 +1,18 @@
+import classes from '*.module.css';
+import { FormControl, InputLabel, Select } from '@material-ui/core';
 import './ChoiceExercise.css';
 
-export function ChoiceExercise()
+export function ChoiceExercise(isMultipleChoice : boolean)
 {
     return (
         <div>
-            
+            <FormControl variant="outlined" className={classes.FormControl}>
+                <InputLabel>Antwort</InputLabel>
+                <Select>
+                    
+                </Select>
+                <menuitem></menuitem>
+            </FormControl>
         </div>
     );
 }
