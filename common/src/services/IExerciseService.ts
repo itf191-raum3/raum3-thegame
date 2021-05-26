46fb1d92970b19c891895e33a33c94ab938bc063
Justin Braun <justinbraun1999@web.de>
2021-05-26 10:16:43 +0200

    * Updated ExerciseService Interface

--- common/src/services/IExerciseService.ts
+++ common/src/services/IExerciseService.ts
@@ -3,9 +3,11 @@ import {IExercise} from "../entities/IExercise";
 export interface IExerciseService {
     createExercise(exercise: IExercise): Promise<void>;
     
-    deleteExercise(exercise: IExercise): Promise<void>;
+    deleteExercise(id: string): Promise<void>;
 
     editExercise(exercise: IExercise): Promise<void>;
+
+    getExercise(id: string): Promise<IExercise>;
 
     getSubjectExercisesByLabel(subjectLabel: string): Promise<Array<IExercise>> ;
 }
