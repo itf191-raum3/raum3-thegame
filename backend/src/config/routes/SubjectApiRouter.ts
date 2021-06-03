e45df0d4686ea363f6579228f07e11dabac6a184
chipskater <nico.matzutt@gmail.com>
2021-06-03 14:28:49 +0200

    refactored routes; added error handling

---
+++ backend/src/config/routes/SubjectApiRouter.ts
@@ -0,0 +1,24 @@
+import { ApiRoute } from "../../../types/common"
+import { NextFunction, Request, Response } from "express";
+import { SubjectService } from "@/service/SubjectService";
+
+const subjectService = new SubjectService();
+
+export const getExercisesForSubject = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const subject = await subjectService.getSubjectById(<string>req.query.id);
+    return res.send({
+      exercises: subject.IExercises
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+export const exerciseApi : Array<ApiRoute> = [
+  {
+    path: "/subject/exercises",
+    method: "GET",
+    handler: getExercisesForSubject
+  }
+]
