2f40dcdb676d0023749122a6ad408da68dad1c98
Niko <nikofahl@outlook.com>
2021-06-03 23:02:25 +0200

    feat: Add first setup for backend communication

---
+++ frontend/src/widgets/choice/choiceApiTool.ts
@@ -0,0 +1,20 @@
+import { IChoice } from '../../../../common/src/entities/IChoice';
+
+export function requestChoiceExerciseFromBackend(myChoiceExercise: IChoice)
+{
+    return fetch('localhost/', { method: 'GET', body: JSON.stringify({myChoiceExercise})})
+    .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return Promise.reject(response.status + ' ' + response.statusText);
+        }
+      })
+      .then((json) => {
+        if (Array.isArray(json) && typeof json[0] === 'string') {
+          return json as string[];
+        } else {
+          return Promise.reject('The Format is not supported');
+        }
+      });
+    }
