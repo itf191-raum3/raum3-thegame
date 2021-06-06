6103681ce999cf25e0386f732eb333451d95f309
Justin Braun <justinbraun1999@web.de>
2021-06-06 18:43:01 +0200

    * Extracted Interface from GameSessionService

--- backend/src/service/GameSessionService.ts
+++ backend/src/service/GameSessionService.ts
@@ -3,15 +3,16 @@ import {Subject} from "@/entities/Subject";
 import {getManager} from "typeorm";
 import {Exercise} from "@/entities/Exercise";
 import {sample} from "lodash";
+import {IGameSessionService} from "@common/services/IGameSessionService";
 
-export class GameSessionService {
+export class GameSessionService implements IGameSessionService {
     async createGameSession(subject: Subject): Promise<GameSession> {
         let gameSession = new GameSession(subject);
         await getManager().insert(GameSession, gameSession);
         return gameSession;
     }
 
-    private async getRandomExercise(gameSession: GameSession): Promise<Exercise | undefined> {
+    async getRandomExercise(gameSession: GameSession): Promise<Exercise | undefined> {
         const randomDifficulty = Math.random() * (gameSession.maxDifficulty - 1) + 1;
         const possibleExercises = gameSession.currentSubject.exercises.filter(exercise => exercise.difficulty <= randomDifficulty);
         return sample(possibleExercises);
