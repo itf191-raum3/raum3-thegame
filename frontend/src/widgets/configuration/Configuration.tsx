fea65bda966d2b0ec01d92b969cf1f65a2f14788
Timo <Timo.Theissen@outlook.com>
2021-06-06 14:18:27 +0200

    reworked configuration gui -> onclick events runs

--- frontend/src/widgets/configuration/Configuration.tsx
+++ frontend/src/widgets/configuration/Configuration.tsx
@@ -1,10 +1,11 @@
-import deleteIcon from './delete_icon.png';
-import editIcon from './edit_icon.png';
+import './Configuration.css'
+import deleteIcon from './delete_icon.png'
+import editIcon from './edit_icon.png'
 import acceptIcon from './accept_icon.png'
 import addIcon from './add_icon.png'
-import './Configuration.css'
+
 import { IExercise } from '../../../../common/src/entities/IExercise';
-import { renderToStaticMarkup } from "react-dom/server"
+import { useState } from 'react';
 
 
 //fetch API()
@@ -14,198 +15,185 @@ import { renderToStaticMarkup } from "react-dom/server"
 // statusCode update (Aufgabe)
 // UUID create (Aufgabe) 
 const columnNames = ["Schwierigkeit", "Aufgabe", "Richtige Antworten", "Antwortmöglichkeiten", "", ""]
-var allExercises = new Array<IExercise>()
 
-function loadCreateTable(){
-  var emptyTable = document.getElementById("workingTable");
-  emptyTable?.getElementsByTagName("tbody").item(0)?.remove()
+export function Configuration() {
+  const [allExercises, setallExercises] = useState<IExercise[]>(Array<IExercise>())
+  const [workingTable, setWorkingTable] = useState<JSX.Element>()
 
-  var manantoryRows = 
-    <tr className="information">
-      <td><input type="number" min="0" id="difficulty" placeholder="Schwierigkeit"/></td>
-      <td><input type="text" id="label" placeholder="Aufgabenstellung"/></td>
-      <td><input type="text" id="correctAnswers" placeholder="Richtige Antworten"/></td>
-      <td><input type="text" id="allChoices" placeholder="Antwortmöglichkeiten"/></td>
-      <td><img src={deleteIcon} alt="Löschen" className="bntLogo" onClick={() => (cancelEditing())}/></td>
-      <td><img src={addIcon} alt="Bearbeiten" className="bntLogo"onClick={() => (addDataSet())}/></td>
-    </tr>
+  function loadCreateTable(){
+    var createTabel = 
+      <table id="editTabel">
+        <thead>
+          {generateHeader()}
+        </thead>
+        <tbody>
+          <tr className="information">
+            <td><input type="number" min="0" id="difficulty" placeholder="Schwierigkeit" value=""/></td>
+            <td><input type="text" id="label" placeholder="Aufgabenstellung" value=""/></td>
+            <td><input type="text" id="correctAnswers" placeholder="Richtige Antworten" value=""/></td>
+            <td><input type="text" id="allChoices" placeholder="Antwortmöglichkeiten" value=""/></td>
+            <td><img src={deleteIcon} alt="Löschen" className="bntLogo" onClick={() => (cancelEditing())}/></td>
+            <td><img src={addIcon} alt="Bearbeiten" className="bntLogo"onClick={() => (addDataSet())}/></td>
+          </tr>
+        </tbody>
+      </table>
 
-  var tabelBody = renderToStaticMarkup(<tbody>{manantoryRows}</tbody>)
-  emptyTable?.insertAdjacentHTML('beforeend', tabelBody)
-}
-
-function loadEditTable(idStr:string){
-  const exercise = getExerciseInList(idStr)
-  const {difficulty, label, correctAnswers, possibleAnswers} = exercise
-  var manantoryRows = 
-       <tr className="information">
-        <td><input type="number" min="0" id="difficultyChange" value={difficulty+""}/></td>
-        <td><input type="text" id="labelChange" value={label}/></td>
-        <td><input type="text" id="correctAnswers" value={correctAnswers.join("; ")}/></td>
-        <td><input type="text" id="allChoices" value={possibleAnswers.join("; ")}/></td>
-        <td><img src={deleteIcon} alt="Löschen" className="bntLogo" onClick={() => (cancelEditing())}/></td>
-        <td><img src={acceptIcon} alt="Bearbeiten" className="bntLogo"onClick={() => (changeData())}/></td>
-      </tr>
-
-  var optionalRow =
-    <tr className="moreAnswers">
-      <td/>
-      <td/>
-      <td className="correctAnswers"><input type="text" id="correctAnswers" placeholder="Richtige Antworte"/></td>
-      <td className="allChoices"><input type="text" id="allChoices" placeholder="Antwortmöglichkeit"/></td>
-    </tr>
-
-    return(manantoryRows)
-}
-
-function getExerciseInList(exerciseID: string): IExercise{
-  var index = getIndexByExerciseID(exerciseID)
-  return allExercises[index]
-}
-
-function getIndexByExerciseID(IdStr: string){
-  var index = -1
-
-  for (var exerciseIndex = 0; exerciseIndex < allExercises.length; exerciseIndex++) {
-    var exercise = allExercises[exerciseIndex]
-    if(exercise.id === IdStr){
-      index = exerciseIndex
-    }
+    return(createTabel)
   }
 
-  return index
-}
-
-function loadTableStructure(tableName: string){
-  return(
-    <table id={tableName}>
-      <thead>
-        {generateHeader()}
-      </thead>
-      <tbody>
-
-      </tbody>
-    </table>
-  );
-}
-
-function renderShowingTabel(tableName:string){
-  return(
-    <table id={tableName}>
-      <thead>
-        <tr>
+  function loadEditTable(idStr:string){
+    const exercise = getExerciseInList(idStr)
+    const {difficulty, label, correctAnswers, possibleAnswers} = exercise
+    
+    var editTabel = 
+      <table id="editTabel">
+        <thead>
           {generateHeader()}
-        </tr>
-      </thead>
-      <tbody>
-        {generateBody()}
-      </tbody>
-    </table>
-  )
-}
+        </thead>
+        <tbody>
+          <tr>
+            <td><input type="number" min="0" id="difficulty" placeholder="Schwierigkeit" value={difficulty}/></td>
+            <td><input type="text" id="label" placeholder="Aufgabenstellung" value={label}/></td>
+            <td><input type="text" id="correctAnswers" placeholder="Richtige Antworten" value={correctAnswers.join("; ")}/></td>
+            <td><input type="text" id="allChoices" placeholder="Antwortmöglichkeiten" value={possibleAnswers.join("; ")}/></td>
+            <td><img src={deleteIcon} alt="Löschen" className="bntLogo" onClick={() => (cancelEditing())}/></td>
+            <td><img src={acceptIcon} alt="Bearbeiten" className="bntLogo"onClick={() => (changeData())}/></td>
+          </tr>
+        </tbody>
+      </table>
+    
+    setWorkingTable(editTabel)
+  }
 
-function generateHeader(){
-  var headerRow: JSX.Element[] = []
+  function getExerciseInList(exerciseID: string): IExercise{
+    var index = getIndexByExerciseID(exerciseID)
+    return allExercises[index]
+  }
 
-  columnNames.forEach(columnName => {
-    headerRow.push(<th id={columnName}>{columnName}</th>)
-  });
+  function getIndexByExerciseID(IdStr: string){
+    var index = 0
 
-  return headerRow;
-}
+    for (var exerciseIndex = 0; exerciseIndex < allExercises.length; exerciseIndex++) {
+      var exercise = allExercises[exerciseIndex]
+      if(exercise.id === IdStr){
+        index = exerciseIndex
+      }
+    }
 
-function generateBody(){
-  var dataRows: JSX.Element[] = []
+    return index
+  }
 
-  for (var exerciseIndex = 0; exerciseIndex < allExercises.length; exerciseIndex++) {
-    const {id, difficulty, label, correctAnswers, possibleAnswers} = allExercises[exerciseIndex];
-    dataRows.push(
-      <tr id={id}>
-        <td key={difficulty}>{difficulty}</td>
-        <td key={label}>{label}</td>
-        <td key={correctAnswers.join("")}>{correctAnswers.join("<br/>")}</td>
-        <td key={possibleAnswers.join("")}>{possibleAnswers.join("<br/>")}</td>
-        <td><img src={deleteIcon} alt="Löschen" className="bntLogo" onClick={deleteDataSet(id)}/></td>
-        <td><img src={editIcon} alt="Bearbeiten" className="bntLogo" onClick={editDataSet(id)}/></td>
-      </tr>
+  function renderShowingTabel(){
+    return(
+      <table id="showingTabel">
+        <thead>
+          <tr>
+            {generateHeader()}
+          </tr>
+        </thead>
+        <tbody>
+          {generateBody()}
+        </tbody>
+      </table>
     )
   }
 
-  return dataRows;
-}
+  function generateHeader(){
+    var headerRow: JSX.Element[] = []
 
-function loadDataTable() {
-  var emptyTable = document.getElementById("showingTabel");
-  emptyTable?.getElementsByTagName("tbody").item(0)?.remove()
+    columnNames.forEach(columnName => {
+      headerRow.push(<th id={columnName}>{columnName}</th>)
+    });
 
-  var tabelBody = renderToStaticMarkup(<tbody>{generateBody()}</tbody>)
-  emptyTable?.insertAdjacentHTML('beforeend', tabelBody)
-}
+    return headerRow;
+  }
 
-function loadSubject (subject: string){
-  allExercises = [] // TODO: bekomme von server Aufgaben[]
-  allExercises.push(
-    { id: "123",
-      label: "Was!",
-      difficulty: 1,
-      correctAnswers: ["2"],
-      possibleAnswers: ["3"],
+  function generateBody(){
+    var dataRows: JSX.Element[] = []
+
+    allExercises.forEach(exercise => {
+      const {id, difficulty, label, correctAnswers, possibleAnswers} = exercise;
+      
+      dataRows.push(
+        <tr id={id}>
+          <td>{difficulty}</td>
+          <td>{label}</td>
+          <td>{correctAnswers.join("<br/>")}</td>
+          <td>{possibleAnswers.join("<br/>")}</td>
+          <td><img src={deleteIcon} alt="Löschen" className="bntLogo" onClick={() => deleteDataSet(id)}/></td>
+          <td><img src={editIcon} alt="Bearbeiten" className="bntLogo" onClick={() => editDataSet(id)}/></td>
+        </tr>
+      )
+    });
+
+    return dataRows;
+  }
+
+  function loadSubject (subject: string){
+     // TODO: bekomme von server Aufgaben[]
+     var tempList = []
+     tempList.push(
+      { id: "123",
+        label: "Was!",
+        difficulty: 1,
+        correctAnswers: ["2"],
+        possibleAnswers: ["3"],
+      }
+    )
+
+    tempList.push(
+      { id: "Test",
+        label: "Test123!",
+        difficulty: 2,
+        correctAnswers: ["Ja"],
+        possibleAnswers: ["Nei"],
+      }
+    )
+
+    setWorkingTable(loadCreateTable())
+    setallExercises(tempList)
+  }
+
+  function addDataSet(){
+    //TODO: Get values
+    //TODO: Create exercise
+    //TODO: Insert in database -> set ID
+
+    setWorkingTable(loadCreateTable())
+    //loadDataTable()
+  }
+
+  function editDataSet(id: string):any{
+    loadEditTable(id)
+  }
+
+  function deleteDataSet(id: string) : any{
+    var deleteIndex = getIndexByExerciseID(id)
+
+    //TODO: Delete element in database
+    var tempList = new Array<IExercise>()
+    for(var exerciseIndex = 0; exerciseIndex<allExercises.length; exerciseIndex++){
+      if(exerciseIndex !== deleteIndex){
+        tempList.push(allExercises[exerciseIndex])
+      }
     }
-  )
 
-  allExercises.push(
-    { id: "Test",
-      label: "Test123!",
-      difficulty: 2,
-      correctAnswers: ["Ja"],
-      possibleAnswers: ["Nei"],
-    }
-  )
-  loadDataTable()
-}
+    setallExercises(tempList)
+  }
 
-function addDataSet(){
-  //TODO: Get values
-  //TODO: Create exercise
-  //TODO: Insert in database -> set ID
+  function cancelEditing(){
+    setWorkingTable(loadCreateTable())
+  }
 
-  loadCreateTable()
-  //loadDataTable()
-}
-
-function editDataSet(id: string):any{
-  var emptyTable = document.getElementById("workingTable");
-  emptyTable?.getElementsByTagName("tbody").item(0)?.remove()
-
-  var tabelBody = renderToStaticMarkup(<tbody>{loadEditTable(id)}</tbody>)
-  emptyTable?.insertAdjacentHTML('beforeend', tabelBody)
-
-  loadCreateTable()
-}
-
-function deleteDataSet(id: string) : any{
-  var exerciseIndex = getIndexByExerciseID(id)
-
-  //TODO: Delete element in database
-
-  //allExercises.splice(exerciseIndex, 1)
-  document.getElementById(id)?.remove()
-}
-
-function cancelEditing(){
-  loadCreateTable()
-}
-
-function changeData(){
-  //TODO: get new values
-  //TODO: update Database
+  function changeData(){
+    //TODO: get new values
+    //TODO: update Database
 
 
-  loadCreateTable()
-  loadDataTable()
-}
+    setWorkingTable(loadCreateTable())
+    //loadDataTable()
+  }
 
-export function Configuration() {
   return (
     <div className="Configuration">
       <header className="Config-GUI">
@@ -218,11 +206,11 @@ export function Configuration() {
           </ul>
         </div>
         <div >
-          {loadTableStructure("workingTable")}
+          {workingTable}
           <br /><br />
         </div>
         <div>
-          {renderShowingTabel("showingTabel")}
+          {renderShowingTabel()}
         </div>
       </body>
     </div>
