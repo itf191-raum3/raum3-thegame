2776ad977149adc97faee930e2158fd26ea5fc53
Justin Braun <justinbraun1999@web.de>
2021-06-06 22:42:47 +0200

    * Cleaning Config

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -3,10 +3,10 @@ import type {Config} from '@jest/types';
 const {defaults} = require("jest-config");
 
 const config: Config.InitialOptions = {
-  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
-  verbose: true,
-  testMatch: [
-    "**/test/*Test.ts"
-  ]
+    moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
+    verbose: true,
+    testMatch: [
+        "**/test/*Test.ts"
+    ]
 };
 export default config;
