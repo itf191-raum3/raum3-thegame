0c1f0c6b02c554d6dea96ecab9c8a319b0c149ef
Justin Braun <justinbraun1999@web.de>
2021-06-06 18:38:59 +0200

    + Added SeedExercise

---
+++ backend/src/migrations/1622994244231-SeedExercise.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class SeedExercise1622994244231 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+
+    }
+
+}
