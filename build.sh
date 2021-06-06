82f7db887bfb13dd58951bd43533722c4238355d
chipskater <nico.matzutt@gmail.com>
2021-06-07 00:58:40 +0200

    updated build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ cd ../backend
 yarn install
 npm run build
 npm run pack
+cp -r src/public/build/static/media dist/backend/src/public/build/static/media
+cp -r src/public/build/static/css dist/backend/src/public/build/static/css
+cp src/public/build/* dist/backend/src/public/build/
