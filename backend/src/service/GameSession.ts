6bde5d87474118078c4d6c63c18836c164062d59
Justin Braun <justinbraun1999@web.de>
2021-06-06 17:11:45 +0200

    * Updated GameSession

--- backend/src/service/GameSession.ts
+++ backend/src/service/GameSession.ts
@@ -3,22 +3,24 @@ import {Exercise} from "@/entities/Exercise";
 
 export class GameSession {
     sessionId: string;
-    exercisesDone: number;
     currentSubject: string;
     currentDifficulty: number;
     exercisePool: Array<Exercise>;
 
     private exerciseService: ExerciseService = new ExerciseService();
 
-    constructor(currentSubject: string) {
+    constructor(sessionId: string, currentSubject: string) {
+        this.sessionId = sessionId;
         this.currentSubject = currentSubject;
     }
 
-    async getNextExercise(): Promise<Exercise> {
-        if (this.exercisePool.length == 0) {
-            this.currentDifficulty++;
-            this.exercisePool = await this.exerciseService.getSubjectExercisesByDifficulty(this.currentSubject, this.currentDifficulty);
-        }
+    async getNextExercise(): Promise<Exercise | undefined> {
+        if(this.currentDifficulty > 10)
+            return undefined;
+
+        if (this.exercisePool.length == 0)
+            this.exercisePool = await this.exerciseService.getSubjectExercisesByDifficulty(this.currentSubject, ++this.currentDifficulty);
+
         return <Exercise>this.exercisePool.pop();
     }
 }
