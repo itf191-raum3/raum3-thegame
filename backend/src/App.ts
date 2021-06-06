f80e53c63fbac14eaee4f1e3ef1f90534661cba8
Justin Braun <justinbraun1999@web.de>
2021-06-06 22:29:28 +0200

    * Added GameSession Router

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -3,6 +3,7 @@ import express, {Request, Response} from "express";
 import {ApiRouter} from "@/service/router/ApiRouter";
 import {exerciseApi} from "@/config/routes/ExerciseApiRoutes"
 import {subjectApi} from "@/config/routes/SubjectApiRoutes";
+import {gameSessionApi} from "@/config/routes/GameSessionApiRoutes";
 import {PageRouter} from "@/service/router/PageRouter";
 import {pages} from "@/config/routes/PageRoutes";
 import {resolve} from "path";
@@ -12,6 +13,7 @@ export const app = express();
 app.use(express.json());
 app.use("/api/", new ApiRouter(exerciseApi).router);
 app.use("/api/", new ApiRouter(subjectApi).router);
+app.use("/api/", new ApiRouter(gameSessionApi).router);
 
 app.use("/", new PageRouter(pages).router);
 app.use("/", express.static(resolve(__dirname, "./public/build")));
