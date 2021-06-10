00efd8aed43ddf3a92d065dba26a43387cb32ebd
Justin Braun <justinbraun1999@web.de>
2021-06-10 08:04:46 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ cd ../backend
 yarn install
 npm run build
 npm run pack
+mkdir -p src/public/build/static
 cp -r src/public/build/static/media dist/backend/src/public/build/static/media
 cp -r src/public/build/static/css dist/backend/src/public/build/static/css
 cp src/public/build/* dist/backend/src/public/build/
